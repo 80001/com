f10545eb9264f6549577c1b99826469b6bce4d1a
80001 <romik.herman@gmail.com>
2023-01-23 10:48:43 +0200

    ignor changes

--- src/components/directory/Directory.jsx
+++ src/components/directory/Directory.jsx
@@ -34,8 +34,7 @@ const categories = [
 	},
 ]
 
-git config --global user.email "romik.herman@gmail.com"
-  git config --global user.name "80001"
+
 const Directory = () => {
 	return (
 		<div className="directory-container">
